d270adc49f1549bf1fab3be64ca2e703bf028f20
xandredp <xduplooy@gmail.com>
2017-03-01 23:28:57 +1300

    New project
    New Project with assets

---
+++ Source/NewFreshStart/NewFreshStartGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "NewFreshStartGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NEWFRESHSTART_API ANewFreshStartGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
